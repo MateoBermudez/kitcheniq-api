2c1339117202f6aa1c5e2e4968069f2f6a64ecaa
EmmanuelArangoV <emmanuelarangov@gmail.com>
2025-09-10 22:31:33 -0500

    feat(auth): update Employee and Supplier entity for auth

--- src/main/java/com/uni/kitcheniq/models/Employee.java
+++ src/main/java/com/uni/kitcheniq/models/Employee.java
@@ -2,10 +2,23 @@ package com.uni.kitcheniq.models;
 
 import com.uni.kitcheniq.enums.EmployeeType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "employee")
-public class Employee {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements AppUserDetails{
     @Id
     @Column(name = "id", nullable = false)
     private String id;
@@ -17,16 +30,14 @@ public class Employee {
     private String name;
 
     @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     EmployeeType type;
-
-    public String getId() {
-        return id;
-    }
 
     public void setId(String id) {
         this.id = id;
     }
 
+    @Override
     public String getName() {
         return name;
     }
@@ -35,16 +46,48 @@ public class Employee {
         this.name = name;
     }
 
-    public EmployeeType getType() {
-        return type;
-    }
-
     public void setType(EmployeeType type) {
         this.type = type;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(type.name()));
+    }
+
+    @Override
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {

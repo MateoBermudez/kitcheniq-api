0e5c1674b7c63242ef3093d29bc8f80116483cf5
EmmanuelArangoV <emmanuelarangov@gmail.com>
2025-09-10 22:35:59 -0500

    feat(auth): create authentication controller
    Add REST endpoints for login and registration operations

--- src/main/java/com/uni/kitcheniq/controller/AuthController.java
+++ src/main/java/com/uni/kitcheniq/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.uni.kitcheniq.controller;
 
+import com.uni.kitcheniq.dto.AuthResponse;
+import com.uni.kitcheniq.dto.LoginRequest;
+import com.uni.kitcheniq.dto.RegisterRequest;
+import com.uni.kitcheniq.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService AuthService;
+
     @PostMapping("/auth/login")
-    public String login(){
-        return "Login from public API";
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest Request) {
+        return ResponseEntity.ok(AuthService.login(Request));
     }
 
     @PostMapping("/auth/register")
-    public String register(){
-        return "Register from public API";
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest Request) {
+        return ResponseEntity.ok(AuthService.register(Request));
     }
 }
 

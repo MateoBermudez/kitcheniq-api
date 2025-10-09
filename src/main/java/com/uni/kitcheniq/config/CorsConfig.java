5f0a7c3e6c3891334aa8681e091c35b02a1d8dfd
Mateo Bermúdez <159858386+MateoBermudez@users.noreply.github.com>
2025-10-08 21:59:40 -0500

    Add allowed origin for production environment

--- src/main/java/com/uni/kitcheniq/config/CorsConfig.java
+++ src/main/java/com/uni/kitcheniq/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/*")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://kitcheniq.vercel.app")
                         .allowedMethods("")
                         .allowedHeaders("*")
                         .allowCredentials(true);

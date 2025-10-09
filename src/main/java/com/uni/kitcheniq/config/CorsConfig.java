768aea1fb27653277acf2ee3e883cf90919ab39f
MateoBermudez <mateo.bermudez2910@gmail.com>
2025-10-08 22:48:40 -0500

    feat: update CORS config to allow all HTTP methods for production

--- src/main/java/com/uni/kitcheniq/config/CorsConfig.java
+++ src/main/java/com/uni/kitcheniq/config/CorsConfig.java
@@ -13,9 +13,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("")
+                registry.addMapping("/**")
+                        .allowedOrigins("https://kitcheniq.vercel.app")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }

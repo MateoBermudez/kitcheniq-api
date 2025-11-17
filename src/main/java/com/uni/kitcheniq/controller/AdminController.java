a8c099973c94d76456cc4ebeaf837840da4bd5a1
EmmanuelArangoV <emmanuelarangov@gmail.com>
2025-11-17 10:22:09 -0500

    feat(backend): add admin inventory endpoints
    Add endpoints to manage inventory items and create purchase

---
+++ src/main/java/com/uni/kitcheniq/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.uni.kitcheniq.controller;
+
+import com.uni.kitcheniq.dto.InventoryItemDTO;
+import com.uni.kitcheniq.dto.PurchaseOrderDTO;
+import com.uni.kitcheniq.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("kitcheniq/api/v1/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/add-inventory-item")
+    public ResponseEntity<String> addInventoryItem(@RequestBody InventoryItemDTO item) {
+        String response = adminService.addInventoryItem(item);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/inventory-list")
+    public ResponseEntity<List<InventoryItemDTO>> getInventoryItems() {
+        List<InventoryItemDTO> items = adminService.getAllInventoryItems();
+        return ResponseEntity.ok(items);
+    }
+
+    @PostMapping("/create-purchase-order")
+    public ResponseEntity<String> createPurchaseOrder(@RequestBody PurchaseOrderDTO purchaseOrderDTO) {
+        adminService.createPurchaseOrder(purchaseOrderDTO);
+        return ResponseEntity.ok("OK");
+    }
+}
